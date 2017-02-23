1a7d577f9ceda0b86109bf076396a124215e6a1f
Dave Harned <dharned@gmail.com>
2017-02-22 21:29:09 -0600

    successful insertOne

--- server.js
+++ server.js
@@ -8,13 +8,34 @@ var MongoClient = mongodb.MongoClient;
 var app = express();
 
 app.use(bodyParser.json());
-var localdb = 'mongodb://localhost:27017';
-var db;
+var localdb = 'mongodb://localhost:27017/testblog';
 
-MongoClient.connect(localdb, function(err, database) {
+
+MongoClient.connect(localdb, function(err, db) {
     if (err) {
         console.log('Error:', err);
         process.exit(1);
     }
 
+    var server = app.listen('3000', function() {
+        var port = server.address().port;
+        console.log('Express server listening on port', port);
+    });
+
+    var newPost = {
+        "message": "This is the first blog post, hello blargz!",
+        "name": "davi3blue",
+        "createdOn": new Date()
+    };
+
+    db.collection('posts').insertOne(newPost, function(err, doc) {
+        if (err) {
+            console.log('Error:', err);
+        } else {
+            console.log(doc.ops[0]);
+        }
+    })
+
+
 });
+
