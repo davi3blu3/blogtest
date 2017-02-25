5c6db90bd2c490fdf78cfcee36a9ed8a9e00b703
Dave Harned <dharned@gmail.com>
2017-02-25 12:51:29 -0600

    http delete working

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,12 +1,20 @@
-
 angular.module('myApp', []).controller('MyController', MyController);
 
-function MyController($http){
-    var vm = this;
-    vm.title = 'microBlog suggah';
+function MyController($scope, $http){
 
     $http.get('/posts').then(function(response) {
         $scope.posts = response.data;
         console.log($scope.posts);
     })
+
+    $scope.handleDelete = function(postID) {
+        console.log('Delete Clicked!', postID);
+
+        $http.delete('/posts/' + postID).then(function(response){
+            console.log(response.status);
+            // $scope.$apply();
+        })
+
+    }
 }
+
