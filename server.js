46cd7c9504d8560aacc6a3770776b93df316b4fb
Dave Harned <dharned@gmail.com>
2017-02-26 20:20:56 -0600

    add comments, clean up console logs

--- server.js
+++ server.js
@@ -1,18 +1,24 @@
+// bring in dependencies
 var express = require('express');
+var app = express();
 var path = require('path');
 var bodyParser = require('body-parser');
+
+// database specific dependencies, methods, and path
 var mongodb = require('mongodb');
 var ObjectID = mongodb.ObjectID;
-var MongoClient = mongodb.MongoClient;
 var localdb = 'mongodb://localhost:27017/testblog';
 
-// setup server
-var app = express();
+// setup middleware
 app.use(express.static(__dirname + '/public'));
+app.use(function(req, res, next) {
+    console.log(req.method, req.url);
+    next();
+})
 app.use(bodyParser.json());
 
 // connect to database
-MongoClient.connect(localdb, function(err, db) {
+mongodb.MongoClient.connect(localdb, function(err, db) {
     if (err) {
         console.log('Mongo Connect error:', err);
         process.exit(1);
@@ -35,7 +41,6 @@ MongoClient.connect(localdb, function(err, db) {
         db.collection('posts').find({}).toArray(function(err, docs) {
             if (err) errorHandler(err);
             else {
-                console.log('GET request received!');
                 res.status(200).json(docs);
             }
         });
@@ -53,9 +58,7 @@ MongoClient.connect(localdb, function(err, db) {
         db.collection('posts').insertOne(newPost, function(err, doc) {
             if (err) errorHandler(err);
             else {
-                console.log('POST request received!');
-                console.log(doc.ops[0]);
-                res.status(200).send(doc.ops[0]);
+                res.status(200).send();
             }
         })
     })
@@ -71,7 +74,6 @@ MongoClient.connect(localdb, function(err, db) {
          db.collection('posts').updateOne({_id: new ObjectID(req.params.id)}, updateDoc, function(err, doc) {
              if (err) errorHandler(err);
              else {
-                 console.log('PUT request received!');
                  res.status(204).send();
              }
          })
@@ -82,7 +84,6 @@ MongoClient.connect(localdb, function(err, db) {
         db.collection('posts').deleteOne({_id: new ObjectID(req.params.id)}, function(err, result) {
             if (err) errorHandler(err);
             else {
-                console.log('DELETE request received!');
                 res.status(204).send()
             }
         })
