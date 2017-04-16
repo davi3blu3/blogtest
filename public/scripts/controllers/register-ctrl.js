91e9f8aa632cde51be1bbc028f97a6013ae04e45
Dave Harned <dharned@gmail.com>
2017-04-15 20:50:31 -0500

    form data to controller

--- public/scripts/controllers/register-ctrl.js
+++ public/scripts/controllers/register-ctrl.js
@@ -1,7 +1,15 @@
 angular.module('myApp')
-    .controller('RegisterController', function RegisterController($routeParams){
+    .controller('RegisterController', function RegisterController($scope){
     // empty object to hold form data from edit post
     // $scope.editData = {};
 
-    console.log('Register controller loaded');
+    $scope.register = function(){
+        console.log('Register submit button pressed');
+        console.log('username:', $scope.regUsername);
+        console.log('password:', $scope.regPassword);
+
+        // clear form
+        $scope.regUsername = '';
+        $scope.regPassword = '';
+    }
 });
