21fbd7fbbd361cfaddeffbdf19db427a925b5ec3
Dave Harned <dharned@gmail.com>
2017-04-20 18:47:07 -0500

    remove npm-debug.log

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ var express = require('express');
 var app = express();
 var path = require('path');
 var bodyParser = require('body-parser');
-var mongoose = require('mongoose');
 
 // local dependencies
 var db = require('./api/data/dbconnection.js');
