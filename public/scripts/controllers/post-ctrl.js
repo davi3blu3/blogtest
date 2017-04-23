e9ec662501e3bcace34de1274eca3a660f21b81a
Dave Harned <dharned@gmail.com>
2017-04-22 22:07:03 -0500

    post working

--- public/scripts/controllers/post-ctrl.js
+++ public/scripts/controllers/post-ctrl.js
@@ -1,12 +1,12 @@
 angular.module('myApp')
-    .controller('PostController', function PostController($scope, $http){
+    .controller('PostController', function PostController($scope, $http, $location){
 
     // handles new post Submit event
     $scope.submit = function() {
         // console.log($scope.postData);
         console.log('post page submit was called');
         // send POST request with new post
-        $http.post('/posts', $scope.postData).then(function(response){
+        $http.post('/sitv/posts', $scope.postData).then(function(response){
 
             // reload data on success, clear form, return to feed
             if (response.status > 199 && response.status < 300) {           
