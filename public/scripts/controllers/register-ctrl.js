0f946b391fe7d661cd57e58d68741c179d52beb6
Dave Harned <dharned@gmail.com>
2017-04-24 22:38:06 -0500

    rework registration vien view with error messages

--- public/scripts/controllers/register-ctrl.js
+++ public/scripts/controllers/register-ctrl.js
@@ -1,6 +1,9 @@
 angular.module('myApp')
     .controller('RegisterController', function RegisterController($scope, $http){
 
+    $scope.error = undefined;
+    $scope.message = undefined;
+
     $scope.register = function(){
 
         // create user object
@@ -9,15 +12,29 @@ angular.module('myApp')
             password: $scope.regPassword
         };
 
-        // post request
-        $http.post('/sitv/newuser', user)
-            .then(function(response) {
-                console.log(response);
-        })
+        if (!$scope.regUsername || !$scope.regPassword){
+            $scope.error = "Please create a username and a password";
+        } else {
+            if ($scope.regPassword !== $scope.regPasswordRepeat){
+                $scope.error = "Passwords do not match"
+            } else {
+
+                // post request
+                $http.post('/sitv/newuser', user)
+                    .then(function(response) {
+                        console.log(response);
+                        $scope.message = "Registered successfully! Please login.";
+                        $scope.error = undefined;
+                })
+
+                // clear form
+                $scope.regUsername = '';
+                $scope.regPassword = '';
 
 
-        // clear form
-        $scope.regUsername = '';
-        $scope.regPassword = '';
+            }
+        }
+
+
     }
 });
