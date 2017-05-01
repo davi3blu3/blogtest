8d287c59082dc2fd41e3ea37792760b1080f57b9
Dave Harned <dharned@gmail.com>
2017-04-30 21:33:53 -0500

    handle async success message before blanking form

--- public/scripts/controllers/register-ctrl.js
+++ public/scripts/controllers/register-ctrl.js
@@ -3,33 +3,34 @@ angular.module('myApp')
 
     $scope.error = undefined;
     $scope.message = undefined;
-
     $scope.register = function(){
         // create user object
         var user = {
             username: $scope.regUsername,
             password: $scope.regPassword
         };
-
+        // basic validation
         if (!$scope.regUsername || !$scope.regPassword){
             $scope.error = "Please create a username and a password";
         } else {
             if ($scope.regPassword !== $scope.regPasswordRepeat){
-                $scope.error = "Passwords do not match"
+                $scope.error = "Passwords do not match";
+                clearRegInput();
             } else {
                 // post request
                 $http.post('/sitv/newuser', user)
                     .then(function(response) {
-                        $scope.message = "Registered successfully! Please <a href='#/login'>login</a>.";
                         $scope.error = undefined;
+                        $scope.message = "Registered successfully! Please <a href='#/login'>login</a>.";
+                        clearRegInput();
                 })
-                // clear form
-                $scope.regUsername = '';
-                $scope.regPassword = '';
-                $scope.regPasswordRepeat = '';
             }
         }
+    }
 
-
+    clearRegInput = function(){
+        $scope.regUsername = '';
+        $scope.regPassword = '';
+        $scope.regPasswordRepeat = '';
     }
 });
